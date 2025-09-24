013c3c81c25eb04e5afb6201b466034328f1b55d
jreiner16 <joereiner16@gmail.com>
2025-09-24 10:42:19 -0700

    GP-2.4.1&2: Consolidated tester and main, updated to add stretch methods

--- Main.java
+++ Main.java
@@ -2,8 +2,25 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // initializes repo
         Git.initializeRepo("ProjectFolder");
+        // makes test files
         Utils.makeFile("ProjectFolder/CoolFile", "RANDOM DATA");
+        Utils.makeFile("ProjectFolder/otherCoolFile", "ASDFFFF");
+        // blob and index the files
         Git.refresh("ProjectFolder");
+
+        // verify they match
+        System.out.println("INDEX FILE CONTENTS: \n" + Utils.readFile("ProjectFolder/git/index"));
+        System.out.println("HASH OF COOLFILE: " + Utils.SHA1(Utils.readFile("ProjectFolder/CoolFile")));
+        System.out.println("HASH OF OTHERCOOLFILE: " + Utils.SHA1(Utils.readFile("ProjectFolder/otherCoolFile")));
+
+        // deleting the files for those weird-ahh stretch goals. delete this if you want
+        // to see the actual stuff it creates
+        clearFiles("ProjectFolder");
+    }
+
+    public static void clearFiles(String repo) throws IOException {
+        Utils.deleteDirectory(repo);
     }
 }
